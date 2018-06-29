e058ec843462dca8a01400ded5fb43f28a4cc3c3
pavloid <karavaev.pavel.sergeevich@gmail.com>
2018-06-29 18:11:57 +0300

    add Spring MVC

--- src/main/java/com/cryptoeagle/controller/WelcomeController.java
+++ src/main/java/com/cryptoeagle/controller/WelcomeController.java
@@ -1,40 +1,17 @@
 package com.cryptoeagle.controller;
 
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-
-public class WelcomeController extends HttpServlet {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+@Controller
+public class WelcomeController {
 
 
-    }
+    @RequestMapping("/")
+   public String welcome(){
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String name = (String) req.getParameter("name");
-        String email = (String) req.getParameter("email");
-        String password = (String) req.getParameter("password");
-
-        HttpSession session = req.getSession();
-
-
-        req.setAttribute("visible", true);
-//        req.setAttribute("visible", false);
-
-        req.getRequestDispatcher("/views/welcome.jsp").forward(req, resp);
-    }
+       return  "welcome";
+   }
 }
